9eb051d20dd72d53eda4d22da510e864c1eee49f
Lifted Palestino <108762440+LiftedP@users.noreply.github.com>
2022-07-26 09:47:03 -0400

    Add files via upload

---
+++ sdk_labo1_4/514sdk_labo1_4/Program.cs
@@ -0,0 +1,26 @@
+﻿//On roule l'application non-stop, tant que CTRL+C
+using LibrairieConversionNote;
+
+while (true)
+{
+    Console.Write("Type de conversion à faire : ");
+    var input = Console.ReadLine();
+
+    //Si l'utilisateur tape note
+    if (input?.ToUpperInvariant() == "NOTE")
+    {
+        //On demande en boucle la conversion 
+        while (true)
+        {
+            Console.Write("Conversion à faire : ");
+            var note = Console.ReadLine();
+
+            ConversionAlphabetiqueToNote conversion = new ConversionAlphabetiqueToNote();
+
+
+            string noteRetour = conversion.Transformer(note);
+            Console.Write(noteRetour);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ sdk_labo1_4/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -0,0 +1,39 @@
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public string Transformer(string note) { 
+        
+if (note == "A")
+
+            {
+             return "LA"; 
+            }
+if (note == "B")
+            {
+                return "SI";
+            }
+if (note == "C")
+            {
+                return "DO";
+            }
+if (note == "D")
+            {
+                return "RE";
+            }
+if (note == "E")
+            {
+                return "MI";
+            }
+if (note == "F")
+            {
+                return "FA";
+            }
+if (note == "G")
+            {
+                return "SOL";
+            }
+else return "Erreur, entrer une lettre entre A et G";
+        }
+    }
+}
