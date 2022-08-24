1edf26265cd530ae1608088c5f77986a018c3e6a
Lifted Palestino <108762440+LiftedP@users.noreply.github.com>
2022-08-24 19:40:11 -0400

    Update ConversionAlphabetiqueToNote.cs
    Ajout Conversion Note a Alpha

--- sdk_labo1_4/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
+++ sdk_labo1_4/LibrairieConversionNote/ConversionAlphabetiqueToNote.cs
@@ -37,3 +37,46 @@ else return "Erreur, entrer une lettre entre A et G";
         }
     }
 }
+
+
+///« ALPHA» => Note vers alphabétique
+
+﻿namespace LibrairieConversionNote
+{
+    public class ConversionAlphabetiqueToNote
+    {
+        public string Transformer(string Alpha) { 
+        
+if (Alpha == "LA")
+
+            {
+             return "A"; 
+            }
+if (Alpha == "SI")
+            {
+                return "B";
+            }
+if (Alpha == "DO")
+            {
+                return "C";
+            }
+if (Alpha == "RE")
+            {
+                return "D";
+            }
+if (Alpha == "MI")
+            {
+                return "E";
+            }
+if (Alpha == "FA")
+            {
+                return "F";
+            }
+if (Alpha == "SOL")
+            {
+                return "G";
+            }
+else return "Erreur, entrer une lettre entre A et G";
+        }
+    }
+}
